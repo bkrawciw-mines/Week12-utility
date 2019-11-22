c1ddee1e823986e9ccd01e2ae402696b6b70fcd2
bkrawciw-mines <bkrawciw@mines.edu>
2019-11-21 23:05:00 -0700

    Added LoadFile; fixed PrintOutput; added test.txt

--- Week12-utility.py
+++ Week12-utility.py
@@ -7,4 +7,19 @@ Week 12
 
 #Print output
 def PrintOutput(string):
+    string = str(string)
     print('OUTPUT '+string)
+    
+#Load file
+def LoadFile(path):
+    outList = []
+    with open(path) as file:
+        for line in file:
+            outList.append(line.rstrip())
+    return outList
+
+#Load file test
+'''
+lines = LoadFile('test.txt')
+PrintOutput(lines)
+'''
